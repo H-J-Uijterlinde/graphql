4ba6819f76a09a76d130ff29df3f48a47a78bab1
Marcel <moostebring@quintor.nl>
2022-03-21 20:13:06 +0100

    PlaygroundConfiguration & voorgedefinieerde queries.

---
+++ src/main/java/nl/quintor/pokedex/configuration/PlaygroundConfiguration.java
@@ -0,0 +1,35 @@
+package nl.quintor.pokedex.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import graphql.kickstart.autoconfigure.editor.playground.PlaygroundController;
+import graphql.kickstart.autoconfigure.editor.playground.properties.PlaygroundProperties;
+import graphql.kickstart.autoconfigure.editor.playground.properties.PlaygroundTab;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class PlaygroundConfiguration {
+
+    private final ResourceLoader resourceLoader;
+
+    @Bean
+    public PlaygroundController playgroundController(ObjectMapper objectMapper) {
+        var playgroundProperties = new PlaygroundProperties();
+        playgroundProperties.setPageTitle("/pokedex");
+        playgroundProperties.setTabs(playgroundTabs());
+        return new PlaygroundController(playgroundProperties, objectMapper);
+    }
+
+    private List<PlaygroundTab> playgroundTabs() {
+        var playgroundPokemonTab = new PlaygroundTab();
+        playgroundPokemonTab.setName("Queries");
+        playgroundPokemonTab.setQuery(resourceLoader.getResource("classpath:playground/playground_queries.graphqls"));
+
+        return List.of(playgroundPokemonTab);
+    }
+}
